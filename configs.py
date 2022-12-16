258b932c2dc1e3d8de4369c1a67007be3e6b6417
Ezee19 <120289768+Ezee19@users.noreply.github.com>
2022-12-16 15:19:50 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,27 +8,18 @@ class Config(object):
     API_ID = int(os.getenv("API_ID", 15823382))
     API_HASH = os.getenv("API_HASH", "016d5e115a06ddfb6121823d72ae4d8c")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "5918078029:AAHOlk_opieJ0cgVOMxOHdoHufraiaYkZHU")
-    BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "MdiskSearch")
-    USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOHwBu6Rho-wqKxir8UdHtyGcrlOGgKPq3pUGxD-GSWyCdGpM5SYngxTAG8ISeU0PCWkHneWJa-MVU_rUQTNP_7pzdVGrUVmXKVKEPTFCgoGlFEyvBZERjW980WoFRod4Fce-q3nZ17f7cJUXjPxK5j8IXw0lGzgpCKAMK1s88J39iX5gJ2k4vGoQMR6wEZp88mu9zblfWd5CuVlChIjYSgZQSajLHbGsn2GZhoWfgOWs5w21-xVIjbagtz1glq4BsO7GLyhiotazBA10Y6cW1VzFSp1tIVIk0GtzovbsPCxxiVgmRm-teuqz0VypizQ8Hpra6KfnNt7wlLD-Cyp4DQsAJC0=")
+    BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "MdiskSearchRobot")
+    USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOHwBuwiE-fcv9dnvGrdMAj5OvSJ_VoGI_CPWne2OL035Tg8Z_kYyxJpwtoLYorJdjPfqsUAaQBYl1Kvu9w8akyXFQxa3qd7fk7jQqwXVKK7MMLBrrhvpOxKigFbY5MhOIeTl_5ybTGhJXWY63lYS2f_8TvucRebSAQy74WGNl9QkgUSSH_t44gGZ3ktnTYnA6GAl5ufh42u6gdmSspmRjtB_SNHK9W5il4ClPHKo5o24M6KUmoWYxb5q602Gu44cESeZD5UEvY3Od2p8WVisOyt5YMMPL1_D7kh4stoeQOCPhdx_ecsL4Pd8gjEIH6zzSHz-NO-qBEICALT9X9GPahr-kZ4=")
     CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1001631279048))
-    BOT_USERNAME = "Mdisk_search_re_bot"
-    BOT_OWNER = 5104293442
-#    OWNER_USERNAME = os.getenv("tlgbotsowner")
-    BACKUP_CHANNEL = os.getenv("BACKUP_CHANNEL")
+    BOT_USERNAME = os.getenv("BOT_USERNAME", "Mdisk_search_re_bot")
+    BOT_OWNER = int(os.getenv("BOT_OWNER", "5104293442"))
+#    OWNER_USERNAME = os.getenv("OWNER_USERNAME", "tlgbotsowner")
+    BACKUP_CHANNEL = os.getenv("BACKUP_CHANNEL", "-1001696028695")
 #    GROUP_USERNAME = os.getenv("GROUP_USERNAME")
-    START_MSG = os.getenv("START_MSG", '''Hᴇʏ Bᴜᴅᴅʏ! 😃
-
-I'ᴍ A Bᴏᴛ Fᴏʀ Sᴇɴᴅɪɴɢ Fʀᴏᴍ Yᴏᴜʀ Cʜᴀɴɴᴇʟ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ.😚
-
-Yᴏᴜ Cᴀɴ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ.☺️
-
-Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ Cʟɪᴄᴋ Oɴ Hᴇʟᴘ ✅''')
-    START_PHOTO = os.getenv("START_PHOTO", "https://telegra.ph/file/3073c7543fc3ab93659d9.jpg")
-    HOME_TEXT = os.getenv("HOME_TEXT", '''ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕
-
-ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴛʜᴇʀᴇ ʏᴏᴜʀ ʟɪɴᴋꜱ,
-ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏ ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ✅''')
-    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", -1001696028695)
+    START_MSG = os.getenv("START_MSG", 'Just Search 👻')
+    START_PHOTO = os.getenv("START_PHOTO", "https://graph.org/file/54e9d1cf382aaf767dd4f.jpg")
+    HOME_TEXT = os.getenv("HOME_TEXT", 'Just Search 👻')
+    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "-1001696028695")
     DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://W9Uyj61afEXSw601:W9Uyj61afEXSw601@cluster0.zsxso.mongodb.net/?retryWrites=true&w=majority")
     LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001683642795"))
     RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 20))
@@ -38,25 +29,5 @@ Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ Cʟɪᴄᴋ Oɴ Hᴇʟᴘ ✅''')
     AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", 90))
     MDISK_API = os.getenv("MDISK_API", "XXgy4PoF35VECI7kZgTb")
     VERIFIED_TIME  = int(os.getenv("VERIFIED_TIME", "31"))
-    ABOUT_BOT_TEXT = os.getenv("ABOUT_TEXT", '''I ᴏɴʟʏ ꜱʜᴀʀᴇ ᴛʜᴇ ᴘᴏꜱᴛ ꜰʀᴏᴍ ᴘᴇᴏᴘʟᴇ'ꜱ ᴄʜᴀɴɴᴇʟ! ᴡʜᴏ ᴍᴀᴅᴇ ᴍᴇ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ, i ᴅᴏ ɴᴏᴛ ꜱᴛᴏʀᴇ ᴀɴʏ ꜰɪʟᴇꜱ ᴏʀ ᴛᴇxᴛ ɪɴ  ᴍʏ ᴅᴀᴛᴀʙᴀꜱᴇ.✅
-
-ᴅᴍ ꜰᴏʀ ᴀɴʏ Qᴜᴇʀʏ @VK_21264877''')
-    ABOUT_HELP_TEXT = os.getenv("HELP_TEXT", '''🍓 RᴇQᴜɪʀᴇᴍᴇɴᴛ ᴛᴏ ᴜꜱᴇ ᴛʜɪꜱ ʙᴏᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ!
-
-🍓 Sᴛᴇᴘ 1 - Aᴘᴋᴏ ᴇᴋ ɢʀᴏᴜᴘ ᴋɪ ᴊᴀʀᴜʀᴀᴛ ʜᴏɢɪ, ᴊɪꜱᴍᴇ ᴍᴇᴍʙᴇʀꜱ ʙʜɪ ʜᴏ, ᴀᴜʀ ᴇᴋ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴋɪ ᴊᴀʀᴜʀᴀᴛ ʜᴏɢɪ, ᴊɪꜱᴍᴇ ᴀᴘᴋᴇ ꜱᴀʀᴇ ᴘᴏꜱᴛ ʜᴏɴɢᴇ!
-
-🍓 Sᴛᴇᴘ 2 - ʙᴏᴛ ᴋᴏ ᴀᴘɴᴇ ɢʀᴏᴜᴘ ᴀᴜʀ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴋᴀ ᴀᴅᴍɪɴ ʙᴀɴᴀɴᴀ ʜᴏɢᴀ.
-
-🍓 Sᴛᴇᴘ 3 - ɢʀᴏᴜᴘ ᴍᴇ "/request" ᴛʏᴘᴇ ᴋᴀʀ ᴋᴇ ꜱᴇɴᴅ ᴋᴀʀɴᴀ ʜᴏɢᴀ!
-ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ. @VK_21264877
-
-🍓 Sᴛᴇᴘ 4 - ɢʀᴏᴜᴘ ᴍᴇ "/addb - ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ɪᴅ" ᴛʏᴘᴇ ᴋᴀʀ ᴋᴇ ꜱᴇɴᴅ ᴋᴀʀɴᴀ ʜᴏɢᴀ.
-ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ʙʜɪ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ @VK_21264877
-
-🍓 Sᴛᴇᴘ 5 - ᴀʙ ᴀᴘᴋᴏ ᴀᴘɴᴇ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴍᴇ ᴘᴏꜱᴛ ᴅᴀʟɴɪ ʜᴏɢɪ,
-ᴊɪꜱꜱᴇ ᴋɪ ᴀɢᴀʀ ɢʀᴏᴜᴘ ᴍᴇ ᴋᴏɪ ᴜꜱᴇʀ ꜱᴇᴀʀᴄʜ ᴋᴀʀᴇ ᴛᴏ ʏᴇ ʙᴏᴛ ᴜɴ ᴜꜱᴇʀ ᴋᴇ Qᴜᴀʀʏ ᴋᴏ ꜱᴀᴍᴀᴊʜ ᴋᴇ ᴀᴘᴋᴇ ᴄʜᴀɴɴᴇʟ ꜱᴇ ᴘᴏꜱᴛ ᴜᴛʜᴀ ᴋᴇ ᴜɴʜᴇ ᴅᴇ ᴘᴀʏᴇ.
-
-
-🍓 Nᴏᴛᴇ : Bᴏᴛ ᴀᴅᴍɪɴ ᴀᴘᴋᴇ ᴄʜᴀɴɴᴇʟ ᴍᴇ ᴊᴏɪɴ ʜᴏɴᴇ ᴄʜᴀʜɪʏᴇ,
-ᴀɢᴀʀ ʙᴏᴛ ᴀᴅᴍɪɴ ᴀᴘᴋᴀ ʀᴇQᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ɴʜɪ ᴋᴀʀ ʀᴀʜᴇ ʜᴀɪɴ ᴛᴏ ᴜɴʜᴇ ᴘᴇʀꜱᴏɴᴀʟ ᴍꜱɢ ᴋᴀʀᴇɴ.
-👉 @VK_21264877''')
+    ABOUT_BOT_TEXT = os.getenv("ABOUT_TEXT", 'Just Search 👻')
+    ABOUT_HELP_TEXT = os.getenv("HELP_TEXT", 'Just Search 👻')
