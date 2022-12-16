11b27fb8da485e97097a7da7c708f131dff4e0d3
Ezee19 <120289768+Ezee19@users.noreply.github.com>
2022-12-16 11:41:17 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,7 +7,7 @@ import os
 class Config(object):
     API_ID = int(os.getenv("API_ID", 15823382))
     API_HASH = os.getenv("API_HASH", "016d5e115a06ddfb6121823d72ae4d8c")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "5918078029:AAFAuWGqmVOIIp2-VtLQNxD9De6vR9NUgm4")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "5918078029:AAHOlk_opieJ0cgVOMxOHdoHufraiaYkZHU")
     BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "MdiskSearch")
     USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOHwBu6Rho-wqKxir8UdHtyGcrlOGgKPq3pUGxD-GSWyCdGpM5SYngxTAG8ISeU0PCWkHneWJa-MVU_rUQTNP_7pzdVGrUVmXKVKEPTFCgoGlFEyvBZERjW980WoFRod4Fce-q3nZ17f7cJUXjPxK5j8IXw0lGzgpCKAMK1s88J39iX5gJ2k4vGoQMR6wEZp88mu9zblfWd5CuVlChIjYSgZQSajLHbGsn2GZhoWfgOWs5w21-xVIjbagtz1glq4BsO7GLyhiotazBA10Y6cW1VzFSp1tIVIk0GtzovbsPCxxiVgmRm-teuqz0VypizQ8Hpra6KfnNt7wlLD-Cyp4DQsAJC0=")
     CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1001631279048))
@@ -35,7 +35,7 @@ Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ Cʟɪᴄᴋ Oɴ Hᴇʟᴘ ✅''')
     BROADCAST_AS_COPY = os.getenv("BROADCAST_AS_COPY", "False")
     UPDATES_CHANNEL_USERNAME = os.getenv("UPDATES_CHANNEL_USERNAME", "Mdiskbotsupport")
     FORCE_SUB = os.getenv("FORCE_SUB", "False")
-    AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", 120))
+    AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", 90))
     MDISK_API = os.getenv("MDISK_API", "XXgy4PoF35VECI7kZgTb")
     VERIFIED_TIME  = int(os.getenv("VERIFIED_TIME", "31"))
     ABOUT_BOT_TEXT = os.getenv("ABOUT_TEXT", '''I ᴏɴʟʏ ꜱʜᴀʀᴇ ᴛʜᴇ ᴘᴏꜱᴛ ꜰʀᴏᴍ ᴘᴇᴏᴘʟᴇ'ꜱ ᴄʜᴀɴɴᴇʟ! ᴡʜᴏ ᴍᴀᴅᴇ ᴍᴇ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ, i ᴅᴏ ɴᴏᴛ ꜱᴛᴏʀᴇ ᴀɴʏ ꜰɪʟᴇꜱ ᴏʀ ᴛᴇxᴛ ɪɴ  ᴍʏ ᴅᴀᴛᴀʙᴀꜱᴇ.✅
