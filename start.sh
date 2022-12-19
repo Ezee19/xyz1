85c0d3d38393f72a5c26562f15714507d304e289
Ezee19 <120289768+Ezee19@users.noreply.github.com>
2022-12-19 23:44:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/Link-Search-Bot-Pro /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/Ezee19/xyz.git /xyz
+cd /xyz
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
