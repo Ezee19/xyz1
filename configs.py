a99095b2b105bc7c049073674775e1d8d31ebc8b
Ezee19 <sonukumarkrbbu67@gmail.com>
2023-02-06 01:59:05 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,11 +7,11 @@ import os
 class Config(object):
     API_ID = int(os.getenv("API_ID", 15823382))
     API_HASH = os.getenv("API_HASH", "016d5e115a06ddfb6121823d72ae4d8c")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "5591577616:AAHUE8cMGXAVvJmRdBSwr1Jsfpz4Ltt93LU")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "5918078029:AAGZuwJXeKo25B-9oc4zfVuYCTElumBQKJo")
     BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "MdiskSearchRobot")
-    USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOHsBu78cjoIz3i-lvDHZVdj9D5MrYXDOfTcyWRHjtj3MhdlZKRZMEQSY57dDmOXjdF4HXWS-Lp_qz8sFDyH9-S4qe1M-YmDJMd7dYxymOt2hwIGlXT6BMZ0ewNO3CFy0aaJADD4xHt8hr6AUWHWm5ZomT_O47i5Z5wF0jUWv909Xinhbf2Y8h0gWI8okn1lZs-yEkFei3bn52aNsjQDkVK5wWj2-BHBJcx7JpAknofjt0h1e6iPqR9LgUMcBPFtoyRy-LLVwDc2LgfHpk297pKM3VVbDrFQpCQMasYgcT3mpkKy2s1iV0zgcW_FXgzpRhgZkO6xS2aqTeHuFJnJSu2ascZc=")
-    CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1001885587345))
-    BOT_USERNAME = "mdiskdownliadbot"
+    USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOKkBu6t7x-ciEmemPHci_9lzpJ4Fg0Li8PELPb_deGSbUp5luomSDM0zYwu57c8jtLtL7dODyWDugn8yxBN4ATD_fBYwzmF5ueDMYZ65-37zt8g9JNgLl2yMwUY-IgPlJQjv0YZT-E7hiyfuQGZANd0sbdmersmKFyCrsCEA4-jESygylm0LY6oGIUYBFh3Tc4XqSUMyx7wzZUunV3e1jpi0-iy83us6Ugm5N5K6qJzR9l6iZ-N6lRf07O6n5etdbW3-sxIBITQC2eNdBko-p2gcqRb4ncdyPFZQfGJGbensTjXMg5I-rUM4uBNAiJRzSxVJZWfjFQZitaySS2pF-ND-FSo=")
+    CHANNEL_ID = int(os.getenv("CHANNEL_ID", -100188558734))
+    BOT_USERNAME = "Mdisk_search_re_bot"
     BOT_OWNER = 5104293442
 #    OWNER_USERNAME = os.getenv("Ipapcorn_helper")
     BACKUP_CHANNEL = os.getenv("BACKUP_CHANNEL")
@@ -22,7 +22,7 @@ I Am Mdisk Search Bot.😚
 
 You Can Also Add Me To Your Group.☺️
 
-You Can Also Change Your API.
+You Can Also Use Me In Group.
 
 For More Info Click On Help ✅''')
     START_PHOTO = os.getenv("START_PHOTO", "https://telegra.ph/file/3073c7543fc3ab93659d9.jpg")
@@ -32,12 +32,12 @@ For More Info Click On Help ✅''')
 ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏ ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ✅''')
     UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", -1001740189478)
     DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://W9Uyj61afEXSw601:W9Uyj61afEXSw601@cluster0.zsxso.mongodb.net/?retryWrites=true&w=majority")
-    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001683642795"))
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-100167057104"))
     RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 25))
     BROADCAST_AS_COPY = os.getenv("BROADCAST_AS_COPY", "False")
     UPDATES_CHANNEL_USERNAME = os.getenv("UPDATES_CHANNEL_USERNAME", "Film_Update_Official")
     FORCE_SUB = os.getenv("FORCE_SUB", "True")
-    AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", 300))
+    AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", 60))
     MDISK_API = os.getenv("MDISK_API", "UM1dBYJxSu9QO1S9s8Tv")
     VERIFIED_TIME  = int(os.getenv("VERIFIED_TIME", "31"))
     ABOUT_BOT_TEXT = os.getenv("ABOUT_TEXT", '''I ᴏɴʟʏ ꜱʜᴀʀᴇ ᴛʜᴇ ᴘᴏꜱᴛ ꜰʀᴏᴍ ᴘᴇᴏᴘʟᴇ'ꜱ ᴄʜᴀɴɴᴇʟ! ᴡʜᴏ ᴍᴀᴅᴇ ᴍᴇ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ, i ᴅᴏ ɴᴏᴛ ꜱᴛᴏʀᴇ ᴀɴʏ ꜰɪʟᴇꜱ ᴏʀ ᴛᴇxᴛ ɪɴ  ᴍʏ ᴅᴀᴛᴀʙᴀꜱᴇ.✅
@@ -50,10 +50,10 @@ For More Info Click On Help ✅''')
 🍓 Sᴛᴇᴘ 2 - ʙᴏᴛ ᴋᴏ ᴀᴘɴᴇ ɢʀᴏᴜᴘ ᴀᴜʀ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴋᴀ ᴀᴅᴍɪɴ ʙᴀɴᴀɴᴀ ʜᴏɢᴀ.
 
 🍓 Sᴛᴇᴘ 3 - ɢʀᴏᴜᴘ ᴍᴇ "/request" ᴛʏᴘᴇ ᴋᴀʀ ᴋᴇ ꜱᴇɴᴅ ᴋᴀʀɴᴀ ʜᴏɢᴀ!
-ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ. @
+ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ. @Owner_film_bot
 
 🍓 Sᴛᴇᴘ 4 - ɢʀᴏᴜᴘ ᴍᴇ "/addb - ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ɪᴅ" ᴛʏᴘᴇ ᴋᴀʀ ᴋᴇ ꜱᴇɴᴅ ᴋᴀʀɴᴀ ʜᴏɢᴀ.
-ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ʙʜɪ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ @
+ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ʙʜɪ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ @Owner_film_bot
 
 🍓 Sᴛᴇᴘ 5 - ᴀʙ ᴀᴘᴋᴏ ᴀᴘɴᴇ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴍᴇ ᴘᴏꜱᴛ ᴅᴀʟɴɪ ʜᴏɢɪ,
 ᴊɪꜱꜱᴇ ᴋɪ ᴀɢᴀʀ ɢʀᴏᴜᴘ ᴍᴇ ᴋᴏɪ ᴜꜱᴇʀ ꜱᴇᴀʀᴄʜ ᴋᴀʀᴇ ᴛᴏ ʏᴇ ʙᴏᴛ ᴜɴ ᴜꜱᴇʀ ᴋᴇ Qᴜᴀʀʏ ᴋᴏ ꜱᴀᴍᴀᴊʜ ᴋᴇ ᴀᴘᴋᴇ ᴄʜᴀɴɴᴇʟ ꜱᴇ ᴘᴏꜱᴛ ᴜᴛʜᴀ ᴋᴇ ᴜɴʜᴇ ᴅᴇ ᴘᴀʏᴇ.
@@ -62,6 +62,6 @@ For More Info Click On Help ✅''')
 🍓 Note,
 Agar Bot ADMIN Apka Request Accept Nahi Karate Hai To Unse Personal Msg Kare.
 
-👉 @ Or @
+👉 @Owner_film_bot
 
-/api_change To Get MDISK API Change Info''')
+''')
